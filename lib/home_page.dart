c2306933f4f4597dd6bad4414c0b4081220e4039
devkavin <kavindra.senanayake@gmail.com>
2023-04-25 22:06:41 +0530

    modified:   lib/home_page.dart 	modified:   lib/main.dart 	modified:   lib/sql_helper.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -37,6 +37,10 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SQFLite CRUD'),
+      ),
+    );
   }
 }
--- lib/sql_helper.dart
+++ lib/sql_helper.dart
@@ -3,15 +3,13 @@ import 'package:sqflite/sqflite.dart' as sql;
 
 class SQLHelper {
   static Future<void> createTables(sql.Database database) async {
-    await database.execute(
-      """CREATE TABLE items(
+    await database.execute("""CREATE TABLE items(
         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         title TEXT,
         description TEXT,
-        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
       )
-      """,
-    );
+      """);
   }
 
   static Future<sql.Database> db() async {
